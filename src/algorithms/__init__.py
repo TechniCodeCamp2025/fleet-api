7a583f28fa2c879b04e745b4f8c801fe2983ff7c
Lukasz Dabrowski <dabrowskilucas2@gmail.com>
2025-10-30 06:05:07 +0100

    ok3

--- src/algorithms/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Fleet optimization algorithms.
-"""
-from .placement import optimize_placement
-from .assignment import optimize_assignment
-
-__all__ = ['optimize_placement', 'optimize_assignment']
-
